fe88b54c6d9c4514400f91ef63dc378c34deadb7
Vinicius Souza Bezerra <ViniciusS.Bezerra@outlook.com>
2024-05-09 17:03:13 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-3xl">Template</h1>
+    </div>
+
+  )
+}
+
+export default App
