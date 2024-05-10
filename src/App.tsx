7c7735490e6c473a6fc0efce0123927d603931f9
vinicius <viniciussouza2599@gmail.com>
2024-05-09 22:16:04 -0300

    added: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Footer } from "./components/Footer/Footer"
 
 function App() {
 
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-3xl">Template</h1>
-    </div>
+    <>
+      <div className="w-full h-screen flex items-center justify-center">
+        <h1 className="text-3xl">Template</h1>
+
+
+
+
+      </div>
+
+      <Footer />
+    </>
+
+
+
 
   )
 }
---
+++ src/components/Footer/Footer-menu-mobile.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import { Plus } from "../../assets/generic-icons/Plus"
+
+interface AccordionMenuProps {
+    name: string
+    links: string[]
+}
+
+export const AccordionMenu = ({ name, links }: AccordionMenuProps) => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const toogleMenu = () => {
+        return setIsOpen(!isOpen)
+    }
+
+
+    return (
+        <div className="w-full ">
+
+            <button className="w-full flex justify-between items-center text-base font-bold leading-[18px]" onClick={toogleMenu}>{name}  <Plus /></button>
+
+
+            {isOpen && <ul className="flex  flex-col gap-2">
+                {links.map((link) => <li className="text-xs leading-[18px] font-normal">{link}</li>)}
+            </ul>}
+
+        </div>
+    )
+}
