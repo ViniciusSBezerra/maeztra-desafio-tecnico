d729b39039fb8bb61c3c57b8bf7b10f4759a20dc
vinicius <viniciussouza2599@gmail.com>
2024-05-12 11:44:59 -0300

    removing nav menu in mobile

--- src/components/header/menu-nav/MenuNav.tsx
+++ src/components/header/menu-nav/MenuNav.tsx
@@ -2,7 +2,7 @@ import { News } from "../../../assets/header-icons/News"
 
 export const MenuNav = () => {
     return (
-        <nav className="max-w-[1598px] mx-auto">
+        <nav className="max-w-[1598px] mx-auto hidden lg:block">
             <ul className="flex justify-center gap-[15px] py-[14px] ">
                 <li className=" text-sm leading-[21px] cursor-pointer text-[#FAA500] font-bold w-[147px] text-center flex items-center gap-2" > <News /> Novidades</li>
                 <li className="font-normal text-sm leading-[21px] cursor-pointer text-[#353535] w-[147px] text-center hover:font-semibold hover:text-[#FAA500]">Vestidos</li>
